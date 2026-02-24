8e608956ccfd58499c062ff459f6e9788e084568
Max <kirilukm157@gmail.com>
2026-02-24 23:50:31 +0200

    add task3 lab1

---
+++ laba1/task3.java
@@ -0,0 +1,16 @@
+public class task3 {
+    public static void main(String[] args) {
+        int number = 14;
+        int remainder = number % 2;
+
+        switch (remainder) {
+            case 0:
+                System.out.println(number + " - Парне число");
+                break;
+            case 1:
+            case -1: // Для від'ємних непарних чисел
+                System.out.println(number + " - Непарне число");
+                break;
+        }
+    }
+}
