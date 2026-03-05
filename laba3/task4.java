a0b0fd739faa9fbe8eb252ef7d3d2627242dbec2
Max <kirilukm157@gmail.com>
2026-03-05 12:36:20 +0200

    added task4 for lab3

---
+++ laba3/task4.java
@@ -0,0 +1,17 @@
+public class task4 {
+    public static <T extends Comparable<T>> T findMax(T[] array) {
+        if (array == null || array.length == 0) return null;
+        T max = array[0];
+        for (T element : array) {
+            if (element.compareTo(max) > 0) max = element;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Integer[] nums = {1, 5, 3};
+        String[] words = {"Apple", "Zebra", "Mango"};
+        System.out.println("Max num: " + findMax(nums));
+        System.out.println("Max word: " + findMax(words));
+    }
+}
