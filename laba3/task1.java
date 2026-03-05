82c8603ab72eb39fc66ba5ad804561c8e90cea65
Max <kirilukm157@gmail.com>
2026-03-05 12:26:51 +0200

    added update fot task1 lab3

--- laba3/task1.java
+++ laba3/task1.java
@@ -1,3 +1,4 @@
+import java.util.Map;
 import java.util.*;
 
 class Student {
@@ -15,7 +16,7 @@ class Student {
     }
 }
 
-public class StudentRegistry {
+public class task1 {
     private Map<String, Student> students = new HashMap<>();
 
     public void addStudent(String id, Student s) { students.put(id, s); }
@@ -23,5 +24,17 @@ public class StudentRegistry {
     public Student findStudent(String id) { return students.get(id); }
     public void displayAll() {
         students.forEach((id, s) -> System.out.println(id + ": " + s));
+    }
+
+    public static void main(String[] args) {
+        task1 manager = new task1();
+
+        Student s1 = new Student("1", "Ivan");
+        Student s2 = new Student("2", "Olena");
+
+        manager.addStudent("1", s1);
+        manager.addStudent("2", s2);
+
+        manager.displayAll();
     }
 }
